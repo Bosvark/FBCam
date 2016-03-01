bec7c81562b7df3af61bb51c1578757c2b3d80d9
Dieter Oelofse <dieter@bluebag.co.za>
2016-03-01 20:41:13 +0200

    Basic video output to framebuffer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,122 @@
 #include <iostream>
 #include <stdio.h>
-#if 1
+#include <unistd.h>
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#endif
+//#include <opencv2/core/types.hpp>
 #include "framebuffer.h"
 
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	0
 #define APP_NAME		"FBCam"
 
+int info(void)
+{
+	fb::FrameBuffer fb = fb::FrameBuffer("/dev/fb0");
+
+	if(!fb.isOpen()){
+		perror("Failed to open framebuffer device\n");
+		return -1;
+	}
+
+	cv::VideoCapture cap(0);
+
+	if(!cap.isOpened()){
+		std::cout << "Cannot open video stream" << std::endl;
+		return -2;
+	}
+
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+	std::cout << "Framebuffer width : " << fb.width() << std::endl;
+	std::cout << "Framebuffer height: " << fb.height() << std::endl;
+	std::cout << "Bits per Pixel    : " << fb.bps() << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Video size:" << dWidth << "x" << dHeight << std::endl;
+
+	return 0;
+}
+
+int display_frame(cv::Mat &frame, fb::FrameBuffer &fb)
+{
+	for(int y=0; y<frame.rows; y++)
+	{
+		for(int x=0; x<frame.cols; x++)
+		{
+			cv::Vec3b intensity = frame.at<cv::Vec3b>(y, x);
+			uchar b = intensity.val[0]/8;
+			uchar g = intensity.val[1]/4;
+			uchar r = intensity.val[2]/8;
+
+			unsigned short int t = r<<11 | g << 5 | b;
+			long int location = (y * fb.line_length()) + (x* (fb.bps()/8));
+
+			fb.put(location, t);
+		}
+	}
+
+	return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
 	std::cout << APP_NAME << " " << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
+
+	if(info() < 0)
+		return -1;
+
+	fb::FrameBuffer fb = fb::FrameBuffer("/dev/fb0");
+
+	if(!fb.isOpen()){
+		perror("Failed to open framebuffer device\n");
+		return -1;
+	}
+
+	cv::VideoCapture cap(0);
+
+	if(!cap.isOpened()){
+		std::cout << "Cannot open video stream" << std::endl;
+		return -2;
+	}
+
+	char display_frame_info_flag=1;
+
+	while(1){
+		cv::Mat frame;
+
+		bool bSuccess = cap.read(frame);
+
+		if(!bSuccess){
+			std::cout << "Cannot read frame from video stream" << std::endl;
+			break;
+		}
+
+		if(display_frame_info_flag){
+			display_frame_info_flag = 0;
+
+			std::cout << "Frame Channels:" << frame.channels() << std::endl;
+			std::cout << "Color space:" << frame.type() << std::endl;
+			std::cout << "Step:" << frame.step1() << std::endl;
+			std::cout << "Continues:" << frame.isContinuous() << std::endl;
+
+			switch(frame.depth())
+			{
+				case CV_8U: std::cout << "Depth: CV_8U" << std::endl; break;
+				case CV_8S:  std::cout << "Depth: CV_8S" << std::endl; break;
+				case CV_16U: std::cout << "Depth: CV_16U" << std::endl; break;
+				case CV_16S: std::cout << "Depth: CV_16S" << std::endl; break;
+				case CV_32S: std::cout << "Depth: CV_32S" << std::endl; break;
+				case CV_32F: std::cout << "Depth: CV_32F" << std::endl; break;
+				case CV_64F: std::cout << "Depth: CV_64F" << std::endl; break;
+			}
+		}
+
+		display_frame(frame, fb);
+	}
+
 #if 0
 	fb::FrameBuffer fb = fb::FrameBuffer("/dev/fb0");
 
@@ -48,7 +152,7 @@ int main(int argc, char *argv[])
 		}
 	}
 #endif
-#if 1	// Opencv test code
+#if 0	// Opencv test code
 	cv::VideoCapture cap(0);
 
 	if(!cap.isOpened()){
@@ -56,18 +160,17 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	cv::Size refS = cv::Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH),
-					 (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));
-return 0;
-	int dWidth = refS.width;
-	int dHeight = refS.height;
+//	cv::Size refS = cv::Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH), (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));
 
-//	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
-//	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+//	int dWidth = refS.width;
+//	int dHeight = refS.height;
+
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 
 	std::cout << "Frame size:" << dWidth << "x" << dHeight << std::endl;
 
-//	cv::namedWindow("MyVideo", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("MyVideo", CV_WINDOW_AUTOSIZE);
 
 	while(1){
 		cv::Mat frame;
@@ -79,7 +182,7 @@ return 0;
 			break;
 		}
 
-//		cv::imshow("MyVideo", frame);
+		cv::imshow("MyVideo", frame);
 
 		if(cv::waitKey(30) == 27){
 			std::cout << "Escape key pressed" << std::endl;
